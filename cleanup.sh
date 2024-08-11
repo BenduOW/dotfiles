64abec4261a94762a20a48b27c962afc4e175627
BenduOW <fkrill@web.de>
2024-08-11 19:25:21 +0200

    script to remove files after installing

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo rm -r ~/dotfiles/dmenu/.config/dmenu/*.o
+sudo rm -r ~/dotfiles/dwm/.config/dwm/*.o
+sudo rm -r ~/dotfiles/slstatus/.config/slstatus/*.o
+sudo rm -r ~/dotfiles/st/.config/st/*.o
+
+sudo rm ~/dotfiles/dmenu/.config/dmenu/dmenu
+sudo rm ~/dotfiles/dwm/.config/dwm/dwm
+sudo rm ~/dotfiles/slstatus/.config/slstatus
+sudo rm ~/dotfiles/st/.config/st
