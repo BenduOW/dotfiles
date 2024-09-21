06fde5dc384a8370b905d7fd98057b85574c213c
BenduOW <fkrill@web.de>
2024-09-21 22:45:18 +0200

    just made my bash better

--- bash/.bashrc
+++ bash/.bashrc
@@ -9,6 +9,18 @@ if [ -f /usr/bin/fastfetch ]; then
 	fastfetch
 fi
 
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# Enable bash programmable completion features in interactive shells
+if [ -f /usr/share/bash-completion/bash_completion ]; then
+	. /usr/share/bash-completion/bash_completion
+elif [ -f /etc/bash_completion ]; then
+	. /etc/bash_completion
+fi
+
 alias ls='ls --color=auto'
 alias ll='ls -la --color=auto'
 
@@ -16,12 +28,20 @@ alias grep='grep --color=auto'
 
 alias shut='shutdown'
 
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='trash -v'
+alias cat='bat'
+
 export EDITOR=nvim
 export VISUAL=nvim
 alias vim='nvim'
 
 alias oneofetch='/usr/bin/neofetch'
 alias neofetch='fastfetch'
+
+alias tree='tree -CAhF --dirsfirst'
+alias treed='tree -CAFd'
 
 PS1='[\u@\h \W]\$ '
 
@@ -45,3 +65,4 @@ export LD_PRELOAD=/usr/lib64/libstdc++.so.6
 [[ -r "/usr/share/z/z.sh" ]] && source /usr/share/z/z.sh
 
 eval "$(starship init bash)"
+eval "$(zoxide init bash)"
--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ echo "yay has been successfully installed!"
 
 sleep 2
 
-sudo pacman -Sy xorg xorg-xinit base-devel github-cli firefox neovim fastfetch feh starship lxappearance wget z okular flameshot stow trash-cli --no-confirmation --needed
+sudo pacman -Sy xorg xorg-xinit base-devel github-cli firefox neovim fastfetch feh starship lxappearance wget zoxide okular flameshot stow trash-cli bat bash-completion fzf tree --no-confirmation --needed
 
 stow
 
@@ -42,3 +42,21 @@ cd $HOME/.config/slstatus
 sudo make clean install
 cd $HOME/.config/st
 sudo make clean install
+
+
+FONT_NAME="FiraCodeNerdFont"
+    if fc-list :family | grep -iq "$FONT_NAME"; then
+        printf "%b\n" "${GREEN}Font '$FONT_NAME' is installed.${RC}"
+    else
+        printf "%b\n" "${YELLOW}Installing font '$FONT_NAME'${RC}"
+        # Change this URL to correspond with the correct font
+        FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/latest/FiraCode.zip"
+        FONT_DIR="$HOME/.local/share/fonts"
+        TEMP_DIR=$(mktemp -d)
+        curl -sSLo "$TEMP_DIR"/"${FONT_NAME}".zip "$FONT_URL"
+        unzip "$TEMP_DIR"/"${FONT_NAME}".zip -d "$TEMP_DIR"
+        mkdir -p "$FONT_DIR"/"$FONT_NAME"
+        mv "${TEMP_DIR}"/*.ttf "$FONT_DIR"/"$FONT_NAME"
+        fc-cache -fv
+        rm -rf "${TEMP_DIR}"
+        
