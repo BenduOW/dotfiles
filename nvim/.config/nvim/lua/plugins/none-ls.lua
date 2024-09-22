a1d8f7c3b5bd18bdad2f991d1a0525623259d5bc
BenduOW <fkrill@web.de>
2024-09-22 14:27:57 +0200

    addded python and c formatting, setup needs npm from now on.

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,13 +1,36 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-        },
-      })
-  
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  }
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+        -- C:
+        null_ls.builtins.formatting.clang_format,
+				-- lua:
+				null_ls.builtins.formatting.stylua,
+				-- python:
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.diagnostics.ruff,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
