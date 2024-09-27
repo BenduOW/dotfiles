007df94afee6bacf9ac16d57872947711e643fa2
BenduOW <fkrill@web.de>
2024-09-27 16:21:32 +0200

    seperated yay install from normal install, so you can run install script as sudo

--- install.sh
+++ install.sh
@@ -1,33 +1,5 @@
 #! /bin/bash
 
-# Define the installation directory
-INSTALL_DIR="$HOME/build"
-
-# Create the installation directory if it doesn't exist
-mkdir -p "$INSTALL_DIR"
-
-# Navigate to the installation directory
-cd "$INSTALL_DIR" || { echo "Failed to navigate to $INSTALL_DIR"; exit 1; }
-
-# Clone the yay repository from AUR
-if ! git clone https://aur.archlinux.org/yay-git.git; then
-    echo "Failed to clone yay repository"
-    exit 1
-fi
-
-# Navigate into the yay directory
-cd yay-git || { echo "Failed to navigate to yay-git directory"; exit 1; }
-
-# Build and install yay
-if ! makepkg -si --noconfirm; then
-    echo "Failed to build and install yay"
-    exit 1
-fi
-
-echo "yay has been successfully installed!"
-
-sleep 2
-
 sudo pacman -Sy xorg xorg-xinit base-devel github-cli firefox neovim fastfetch feh starship lxappearance wget zoxide okular flameshot stow trash-cli bat bash-completion fzf tree npm zathura texlive-binextra thunderbird --noconfirm
 
 sleep 1
---
+++ yay.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Define the installation directory
+INSTALL_DIR="$HOME/build"
+
+# Create the installation directory if it doesn't exist
+mkdir -p "$INSTALL_DIR"
+
+# Navigate to the installation directory
+cd "$INSTALL_DIR" || { echo "Failed to navigate to $INSTALL_DIR"; exit 1; }
+
+# Clone the yay repository from AUR
+if ! git clone https://aur.archlinux.org/yay-git.git; then
+    echo "Failed to clone yay repository"
+    exit 1
+fi
+
+# Navigate into the yay directory
+cd yay-git || { echo "Failed to navigate to yay-git directory"; exit 1; }
+
+# Build and install yay
+if ! makepkg -si --noconfirm; then
+    echo "Failed to build and install yay"
+    exit 1
+fi
+
+echo "yay has been successfully installed!"
